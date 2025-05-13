590f3a13f50b066bb604735d8b9725dcd7975417
ZegOn01 <114960697+ZegOn01@users.noreply.github.com>
2025-05-13 09:47:41 -0300

    Add files via upload
    Att 2

--- Notas.py
+++ Notas.py
@@ -1,7 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv("Notas.csv",sep= ";")
+# Carrega o DataFrame
+df = pd.read_csv("Notas.csv", sep=";")
 
+# Exibe o editor de dados
+edited_df = st.data_editor(df)
 
-st.data_editor(df)
+# Adiciona um botão para salvar as alterações
+if st.button("Salvar Alterações"):
+    try:
+        edited_df.to_csv("Notas.csv", sep=";", index=False)
+        st.success("As alterações foram salvas com sucesso!")
+    except Exception as e:
+        st.error(f"Ocorreu um erro ao salvar as alterações: {e}")
